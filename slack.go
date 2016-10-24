606d879a969957e4b7dda8f39994b1dc2ddc5cf9
Catalin Covic <catalin.covic@gmail.com>
2016-10-23 22:25:37 -0400

    got rid of unnecessary console.log

--- slack.go
+++ slack.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"strconv"
 	"strings"
@@ -105,7 +104,6 @@ func userLinkGen(username string) string {
 }
 
 func slackUserResponse(res http.ResponseWriter, data *f1User) {
-	fmt.Println("Data is", data)
 	resp := &SlackResponse{
 		ResponseType: "in_channel",
 	}
